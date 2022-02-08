15d27f9caf60ca8347de6405a115996abbdd86be
Taro Mieno <tmieno2@unl.edu>
2022-02-08 08:53:41 -0600

    final changes before Dave's review

--- Codes/knit_manuscript.R
+++ Codes/knit_manuscript.R
@@ -1,1 +1,9 @@
+
+#* manuscript
 rmarkdown::render(here::here("GitControlled/Writing/manuscript_cea.rmd"))
+
+#* reply to reviewer 1
+rmarkdown::render(here::here("GitControlled/Writing/CEA_2nd_round/replies_to_rev_1.rmd"))
+
+#* reply to reviewer 2
+rmarkdown::render(here::here("GitControlled/Writing/CEA_2nd_round/replies_to_rev_2.rmd"))
