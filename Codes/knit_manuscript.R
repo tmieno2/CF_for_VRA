f972108fb711ecf2119ebf5bfbc59a6e54e2464a
Taro Mieno <tmieno2@unl.edu>
2022-02-10 07:43:12 -0600

    minor edits

--- Codes/knit_manuscript.R
+++ Codes/knit_manuscript.R
@@ -3,7 +3,7 @@
 rmarkdown::render(here::here("GitControlled/Writing/manuscript_cea.rmd"))
 
 #* reply to reviewer 1
-rmarkdown::render(here::here("GitControlled/Writing/CEA_2nd_round/replies_to_rev_1.rmd"))
+rmarkdown::render(here::here("GitControlled/Writing/CEA_2nd_round/replies_rev_1.rmd"))
 
 #* reply to reviewer 2
-rmarkdown::render(here::here("GitControlled/Writing/CEA_2nd_round/replies_to_rev_2.rmd"))
+rmarkdown::render(here::here("GitControlled/Writing/CEA_2nd_round/replies_rev_2.rmd"))
